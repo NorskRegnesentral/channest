d5bab16b46d11d317f34e31e8d83d4540d124327
Vegard Berg Kvernelv <vegard@nr.no>
2020-06-30 09:35:32 +0200

    Updated setup.py to be pypi compatible. Updated version number.

--- setup.py
+++ setup.py
@@ -3,8 +3,22 @@ from setuptools import setup, find_packages
 setup(
     name='channest',
     version=open('channest/VERSION.txt').read(),
-    description='Package',
+    description='Package for estimating channel parameters for ResQml models converted by nrresqml',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     author='Norwegian Computing Center',
+    license='GPL-3.0',
+    url='https://github.com/NorskRegnesentral/channest',
     packages=find_packages(),
-    include_package_data=True,
+    package_data={
+        'channest': ['VERSION.txt']
+    },
+    # include_package_data=True,
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
 )
